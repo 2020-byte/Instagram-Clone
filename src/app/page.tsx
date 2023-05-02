8e1eba42a225d5c92b11f452a0e1cf001db5507d
2020-byte <gmmoon0824@gmail.com>
2023-05-01 20:11:02 -0700

    followingbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,12 @@ export default async function HomePage() {
   
   return (
     <section className='w-full flex flex-col md:flex-row max-w-[850px] p-4'>
-      <div className='w-full basis-3/4'>
+      <div className='w-full basis-3/4 min-w-0'>
+        {/* TODO:min-w-0이 구체적으로 어떻게 작용하는 지 여기서 알아보기 */}
         <FollowingBar/>
         <PostList/>
       </div>
-      <div className='basis-1/4'>
+      <div className='basis-1/4 ml-8'>
         <SideBar user={user}/>
       </div>
     </section>

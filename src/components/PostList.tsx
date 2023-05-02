79cb9c5ace45d60a010f5988dd81a3ab9d347952
2020-byte <gmmoon0824@gmail.com>
2023-05-02 11:25:35 -0700

    Post List Card Refactoring

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,28 +1,27 @@
 'use client'
 import { SimplePost } from '@/model/post';
 import useSWR from 'swr';
-import { GridLoader } from 'react-spinners';
 import PostListCard from './PostListCard';
+import GridSpinner from './ui/GridSpinner';
 
 
 export default function PostList() {
     const {data: posts, isLoading: loading} = useSWR<SimplePost[]>('/api/posts')
-    //console.log(posts);
     return (
         <section>
             {
                 loading && (
                     <div className='text-center mt-32'>
-                        <GridLoader color='red'/>
+                        <GridSpinner color='red'/>
                     </div>
                 )
             }
             {
                 posts && (
                     <ul>
-                        {posts && posts.map(post => (
+                        {posts && posts.map((post, index) => (
                             <li key={post.id} className='mb-4'>
-                                <PostListCard post={post} />
+                                <PostListCard post={post} priority={index <2}/>
                             </li>
                         ))}
                     </ul>
